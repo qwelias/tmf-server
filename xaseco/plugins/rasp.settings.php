73290445de79b5b944af45cdb9c5ff382ff3bb3c
Ilia Baryshnikov <qwelias@gmail.com>
2021-04-08 22:44:17 +0200

    fix history

--- xaseco/plugins/rasp.settings.php
+++ xaseco/plugins/rasp.settings.php
@@ -114,7 +114,7 @@ $maxavg = 10;
 
 //Specifies how large the track history buffer is.
 //If a track that is in the buffer gets requested, it won't be jukeboxed.
-$buffersize = 0;
+$buffersize = 1;
 
 //Specifies the required vote ratio for a TMX /add request to be successful.
 $tmxvoteratio = 0.66;
