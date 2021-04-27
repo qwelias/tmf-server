c3947d6d1bde8294f9a3000aa6ac3d5654506142
Ilia Baryshnikov <qwelias@gmail.com>
2021-04-27 18:02:47 +0200

    feat(xaseco): preserve bestcps on restart

--- xaseco/plugins/plugin.bestcps.php
+++ xaseco/plugins/plugin.bestcps.php
@@ -13,8 +13,6 @@ Author : galaad
 Aseco::registerEvent("onStartup", "initStartupServer");
 Aseco::registerEvent("onCheckpoint", "principale_bestcps");
 Aseco::registerEvent("onNewChallenge", "init_bestcps");
-Aseco::registerEvent('onNewChallenge', 'clear_bestcps');
-Aseco::registerEvent('onNewChallenge', 'loadXmlFile');
 Aseco::registerEvent("onPlayerFinish", "update_finish_time");
 
 #changed from onPlayerFinish for realtime updates --nerve6
@@ -32,11 +30,12 @@ $HiddenFor = array();
 $tab_cp_time = array();
 $finish_time = -1;
 $fic;
+$last_uid;
 
 class cp_time{
 	var $time;
 	var $nickname;
-	
+
 	function cp_time($time, $nickname){
 		$this->time = $time;
 		$this->nickname = $nickname;
@@ -56,28 +55,28 @@ function toggle_click($aseco, $answer) {
 		$command['author'] = $player;
 		chat_bestcps($aseco, $command);
 	}
-} 
+}
 
 #function to handle chat command /bestcps --nerve6
 function chat_bestcps($aseco, $command){
 	global $HiddenFor;
 	$login = $command['author']->login;
-	
+
 	if(isset($HiddenFor[$login])){
 		#if array key is set, user has already disabled this widget, so we need to enable it --nerve6
-		
+
 		#remove user from list of disabled --nerve6
 		unset($HiddenFor[$login]);
 		$aseco->client->query('ChatSendServerMessageToLogin', 'Bestcps widget enabled', $login);
-	} else {	
+	} else {
 		#user is not in list of disabled, so add them --nerve6
 		$HiddenFor[$login] = true;
 		$aseco->client->query('ChatSendServerMessageToLogin', 'Bestcps widget disabled', $login);
-		
+
 		#hide widget --nerve6
 		hide_for_user($aseco, $login);
 	}
-	
+
 }
 
 
@@ -87,16 +86,9 @@ function hide_for_user($aseco, $login){
 	$aseco->client->addCall("SendDisplayManialinkPageToLogin", array($login, $xml, 1, false));
 }
 
-
-
-function loadXmlFile($aseco, $tab){
-	global $fic;
-	$fic = simplexml_load_file('bestcps.xml');
-}
-
 function principale_bestcps($aseco, $param){
 	global $tab_cp_time, $nickname, $time, $cp,	$finish_time, $fic;
-	
+
 	$nb_cp_max = $fic->number;
 	$nickname = $aseco->server->players->player_list[$param[1]]->nickname;
 	$time = $param[2];
@@ -104,7 +96,7 @@ function principale_bestcps($aseco, $param){
 
 	if (((!$tab_cp_time[$cp]) or ($time < $tab_cp_time[$cp]->time)) and ($cp != ($aseco->server->challenge->nbchecks-1)) and ($cp < $nb_cp_max)){
 		$tab_cp_time[$cp] = new cp_time($time, $nickname);
-	}	
+	}
 }
 
 function main_display($aseco, $record){
@@ -116,30 +108,30 @@ function display_best_cps_0($aseco){ // affiche les best cps
 	global $tab_cp_time, $fic, $HiddenFor;
 	$texte;
 	$min	= 0;
-	$sec	= 0; 
-	$cen	= 0; 
-	$i 		= 0; 
+	$sec	= 0;
+	$cen	= 0;
+	$i 		= 0;
 	$place 	= 1; // indice cp
 	$line	= 0; // indice de la ligne pour affichage des CP
 
 	$posx_frame 	= $fic->position[0]->x; //main x position of the widget
 	$posy_frame 	= $fic->position[0]->y; //main y position of the widget
-	$amount_per_line = $fic->newline; 	//Nombre de CPS à afficher par ligne
+	$amount_per_line = $fic->newline; 	//Nombre de CPS ï¿½ afficher par ligne
 	$posx;
 	$posy;
-	
-	
+
+
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123456">';
 	$xml.='<frame posn="' .$posx_frame. ' ' .$posy_frame. '">';
 	$xml.='<format textsize="0.5"/>';
-	
+
 	foreach($tab_cp_time as $value){ //pour chaque enregistrement
-		
+
 		$min = (int) (($value->time) / 60000);
 		$sec = (int) ((($value->time) - $min * 60000) / 1000);
 		$cen = (int) (($value->time - $min*60000 - $sec*1000) / 10);
-		
+
 		#re-written for readability --nerve6
 		$texte = '$z';
 		#added a preceeding 0 to the checkpoint number so times line up better --nerve6
@@ -164,12 +156,12 @@ function display_best_cps_0($aseco){ // affiche les best cps
 		$i++;
 		$place++;
 	}
-	
+
 	$xml.='</frame></manialink>';
-	
+
 	#Make sure people have hidden it
 	if(count($HiddenFor) > 0){
-		
+
 		#instead of showing for all players, look for only ones that are not in the HiddenFor array --nerve6
 		#loop through entire player list --nerve6
 		foreach($aseco->server->players->player_list as $player){
@@ -178,46 +170,46 @@ function display_best_cps_0($aseco){ // affiche les best cps
 				#display the CP times --nerve6
 				$aseco->client->addCall("SendDisplayManialinkPageToLogin", array($player->login, $xml, 0, false));
 			}
-			
-		}			
-		
+
+		}
+
 	} else {
 		#Nobody has the thing hidden, send to all
 		$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false);
 	}
-	
+
 }
 
 function display_best_cps_h($aseco){ // affiche les best cps
 	global $tab_cp_time, $fic;
 	$texte;
 	$min=0;
-	$sec=0; 
-	$cen=0; 
+	$sec=0;
+	$cen=0;
 	$i = 0;
 	$place = 1;
 
 	$posx_frame = $fic->position[0]->x; //main x position of the widget
 	$posy_frame = $fic->position[0]->y; //main y position of the widget
-	$posx;	
-	
+	$posx;
+
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123456">';
 	$xml.='<frame posn="' .$posx_frame. ' ' .$posy_frame. '">';
 	$xml.='<format textsize="1"/>';
-	
+
 	foreach($tab_cp_time as $value){ //pour chaque enregistrement
-		
+
 		$min = (int) (($value->time) / 60000);
 		$sec = (int) ((($value->time) - $min * 60000) / 1000);
 		$cen = (int) (($value->time - $min*60000 - $sec*1000) / 10);
 		$texte ='$z'.$place. '. ';
 		$texte .= "$min:";
-		if ($sec < 10) $texte .= "0";	
+		if ($sec < 10) $texte .= "0";
 		$texte .= $sec.'.';
 		if ($cen < 10) $texte .= "0";
 		$texte .= $cen;
-		
+
 		$posx = $i * 14;
 		$xml.='<quad  posn="' .$posx. ' 0" sizen="14 2.2" halign="center" valign="center" style="Bgs1InRace" substyle="NavButton" />';
 		$posx = $posx-6.25;
@@ -227,7 +219,7 @@ function display_best_cps_h($aseco){ // affiche les best cps
 		$i++;
 		$place++;
 	}
-	
+
 	$xml.='</frame></manialink>';
 	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false); //requete d'affichage
 }
@@ -237,32 +229,32 @@ function display_best_cps_1($aseco){ // affiche les best cps
 	global $tab_cp_time, $fic;
 	$texte;
 	$min=0;
-	$sec=0; 
-	$cen=0; 
+	$sec=0;
+	$cen=0;
 	$i = 0;
 	$place = 1;
 
 	$posx_frame = $fic->position[0]->x; //main x position of the widget
 	$posy_frame = $fic->position[0]->y; //main y position of the widget
-	$posx;	
-	
+	$posx;
+
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123456">';
 	$xml.='<frame posn="' .$posx_frame. ' ' .$posy_frame. '">';
 	$xml.='<format textsize="1"/>';
-	
+
 	foreach($tab_cp_time as $value){ //pour chaque enregistrement
-		
+
 		$min = (int) (($value->time) / 60000);
 		$sec = (int) ((($value->time) - $min * 60000) / 1000);
 		$cen = (int) (($value->time - $min*60000 - $sec*1000) / 10);
 		$texte ='$z'.$place. '. ';
 		$texte .= "$min:";
-		if ($sec < 10) $texte .= "0";	
+		if ($sec < 10) $texte .= "0";
 		$texte .= $sec.'.';
 		if ($cen < 10) $texte .= "0";
 		$texte .= $cen;
-		
+
 		$posy = $i * (-2);
 		$xml.='<quad  posn="0 ' .$posy. '" sizen="14 2.2" halign="center" valign="center" style="Bgs1InRace" substyle="NavButton" />';
 		$posy = $posy+0.1;
@@ -271,7 +263,7 @@ function display_best_cps_1($aseco){ // affiche les best cps
 		$i++;
 		$place++;
 	}
-	
+
 	$xml.='</frame></manialink>';
 	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false); //requete d'affichage
 }
@@ -280,34 +272,34 @@ function display_best_cps_2($aseco){ // affiche les best cps
 	global $tab_cp_time, $fic;
 	$texte;
 	$min=0;
-	$sec=0; 
-	$cen=0; 
+	$sec=0;
+	$cen=0;
 	$i = 0;
 	$place = 1;
 
 	$posx_frame = $fic->position[0]->x; //main x position of the widget
 	$posy_frame = $fic->position[0]->y; //main y position of the widget
-	$posx;	
-	
+	$posx;
+
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123456">';
 	$xml.='<frame posn="' .$posx_frame. ' ' .$posy_frame. '">';
 	$xml.='<format textsize="1"/>';
 	$tmp = $nb * 2+0.2;
 	$xml.='<quad  posn="0 1.1" sizen="14 '.$tmp.'" halign="center" valign="top" style="Bgs1InRace" substyle="NavButton" />';
-	
+
 	foreach($tab_cp_time as $value){ //pour chaque enregistrement
-		
+
 		$min = (int) (($value->time) / 60000);
 		$sec = (int) ((($value->time) - $min * 60000) / 1000);
 		$cen = (int) (($value->time - $min*60000 - $sec*1000) / 10);
 		$texte ='$z'.$place. '. ';
 		$texte .= "$min:";
-		if ($sec < 10) $texte .= "0";	
+		if ($sec < 10) $texte .= "0";
 		$texte .= $sec.'.';
 		if ($cen < 10) $texte .= "0";
 		$texte .= $cen;
-		
+
 		$posy = $i * (-2);
 		$posy = $posy+0.1;
 		$xml.='<label posn="-6.2 ' .$posy. '" sizen="6.5 2" halign="left" valign="center" text="'.$texte.'"/>';
@@ -315,7 +307,7 @@ function display_best_cps_2($aseco){ // affiche les best cps
 		$i++;
 		$place++;
 	}
-	
+
 	$xml.='</frame></manialink>';
 	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false); //requete d'affichage
 }
@@ -324,34 +316,34 @@ function display_best_cps_3($aseco){ // affiche les best cps
 	global $tab_cp_time, $fic;
 	$texte;
 	$min=0;
-	$sec=0; 
-	$cen=0; 
+	$sec=0;
+	$cen=0;
 	$i = 0;
 	$place = 1;
 
 	$posx_frame = $fic->position[0]->x; //main x position of the widget
 	$posy_frame = $fic->position[0]->y; //main y position of the widget
-	$posx;	
-	
+	$posx;
+
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123456">';
 	$xml.='<frame posn="' .$posx_frame. ' ' .$posy_frame. '">';
 	$xml.='<format textsize="1"/>';
 	$tmp = count($tab_cp_time) * 2+0.2;
 	$xml.='<quad  posn="0 1.1" sizen="14 '.$tmp.'" halign="center" valign="top" style="Bgs1InRace" substyle="NavButton" />';
-	
+
 	foreach($tab_cp_time as $value){ //pour chaque enregistrement
-		
+
 		$min = (int) (($value->time) / 60000);
 		$sec = (int) ((($value->time) - $min * 60000) / 1000);
 		$cen = (int) (($value->time - $min*60000 - $sec*1000) / 10);
 		$texte ='$z'.$place. '. ';
 		$texte .= "$min:";
-		if ($sec < 10) $texte .= "0";	
+		if ($sec < 10) $texte .= "0";
 		$texte .= $sec.'.';
 		if ($cen < 10) $texte .= "0";
 		$texte .= $cen;
-		
+
 		$posy = $i * (-2);
 		$posy = $posy+0.1;
 		$xml.='<label posn="-6.2 ' .$posy. '" sizen="6.5 2" halign="left" valign="center" text="'.$texte.'"/>';
@@ -359,29 +351,32 @@ function display_best_cps_3($aseco){ // affiche les best cps
 		$i++;
 		$place++;
 	}
-	
+
 	$xml.='</frame></manialink>';
 	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false); //requete d'affichage
 }
 
-function init_bestcps($aseco, $challenge){ // initialise les variables globales a chaque nouvelle map
-	global $tab_cp_time, $fic;
-	$tab_cp_time = array();
-	
-	#Send label for Toggle BestCPs link to everyone --nerve6
+function show_toggle($aseco) {
 	$xml='<?xml version="1.0" encoding="UTF-8"?>';
 	$xml.='<manialink id="123123457">';
 	$xml.='<frame posn="-25 45.7">';
 	$xml.='<format textsize="0.5"/>';
 	$xml.='<label posn="1 1" sizen="5.5 2" halign="left" valign="center" text="Toggle BestCPs" action="9000"/>';
 	$xml.='</frame></manialink>';
-	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false); 
-
+	$aseco->client->query("SendDisplayManialinkPage", $xml, 0, false);
 }
 
-function clear_bestcps($aseco, $challenge){//efface le widget entre 2 challenges
-	$xml = '<manialink id="123123456"></manialink>';
-    $aseco->client->query("SendDisplayManialinkPage", $xml, 1, false);
+function init_bestcps($aseco, $challenge){ // initialise les variables globales a chaque nouvelle map
+	global $tab_cp_time, $fic, $last_uid;
+	$fic = simplexml_load_file('bestcps.xml');
+
+    if ($last_uid != $challenge->uid) {
+        $tab_cp_time = array();
+    }
+    $last_uid = $challenge->uid;
+
+    show_toggle($aseco);
+    main_display($aseco);
 }
 
 function update_finish_time($aseco, $record){
