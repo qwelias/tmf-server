868285b2d9c29bcbd91a08aec2ac36f3f4363a40
Ilia Baryshnikov <qwelias@gmail.com>
2026-08-04 04:59:19 +0500

    xaseco

--- xaseco/plugins/chat.admin.php
+++ xaseco/plugins/chat.admin.php
@@ -940,9 +940,9 @@ function chat_admin($aseco, $command) {
 								$jukebox[$uid]['tmx'] = false;
 								$jukebox[$uid]['uid'] = $uid;
 								$aseco->releaseEvent('onJukeboxChanged', array('add', $jukebox[$uid]));
-								$message = formatText('{#server}>> {#admin}{1}$z$s {#highlite}{2}$z$s {#admin}jukeboxes track: {#highlite}{3} {#admin}from {5}',
+								$message = formatText('{#server}>> {#admin}{1}$z$s {#highlite}{2}$z$s {#admin}jukeboxes track: {#highlite}{3} {#admin}from {4}',
 								                      $chattitle, $admin->nickname,
-								                      stripColors($track['Name']), $source);
+								                      stripColors($key['name']), $source);
 								$aseco->client->query('ChatSendServerMessage', $aseco->formatColors($message));
 							}
 							unlink($localfile);
